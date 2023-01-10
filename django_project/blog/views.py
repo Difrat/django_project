2e9a310299f3fe4c6408bea7c1a07a1af4c6772c
Difrat <bozhenko_m_v@bk.ru>
2023-01-10 22:12:11 +1000

    I created links to individual

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post2
 
 
 class BlogListView(ListView):
     model = Post2
     template_name = "home.html"
+
+
+class BlogDetailView(DetailView):
+    model = Post2
+    template_name = "post_detail.html"
