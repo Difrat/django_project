e4b82117ac2844007450a704cfd0095910fa1578
Difrat <bozhenko_m_v@bk.ru>
2023-01-12 00:09:30 +1000

    Create Create, Update, Delete View. New Fix BlogDeleteView.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post2
 
 
@@ -10,3 +11,22 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post2
     template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post2
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post2
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DetailView):
+    model = Post2
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
+
